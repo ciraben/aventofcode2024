3f317e17624ecb928ce414df40a185a62432bba4
ciraben <tom.on.github@gmail.com>
2024-12-13 21:08:21 -0800

    Splits one line into columns and finds difference

--- one/solver.rb
+++ one/solver.rb
@@ -1,2 +1,21 @@
 class Day1Solver
+  attr_reader :left_column, :right_column
+
+  def initialize(string)
+    @left_column = []
+    @right_column = []
+
+    string.each_line { |line| capture_row_data(line) }
+  end
+
+  def capture_row_data(row_string)
+    left_val, right_val = row_string.split("   ").map {|val| val.to_i }
+    @left_column.append left_val
+    @right_column.append right_val
+  end
+
+  def total_difference
+    (@left_column[0] - @right_column[0])
+    .abs
+  end
 end
--- spec/day1_spec.rb
+++ spec/day1_spec.rb
@@ -2,5 +2,15 @@ require "debug"
 require_relative "../one/solver"
 
 RSpec.describe Day1Solver do
-  subject { Day1Solver.new }
+  subject { Day1Solver.new(input) }
+
+  context "given an input line" do
+    let(:input) { "3   4" }
+
+    it "converts it to integers stored in columns" do
+      expect(subject.left_column).to eq [3]
+      expect(subject.right_column).to eq [4]
+      expect(subject.total_difference).to eq 1
+    end
+  end
 end
