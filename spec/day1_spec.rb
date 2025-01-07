cdf517b81387e4ca1d3d71578a2482746b1e50f9
ciraben <tom.on.github@gmail.com>
2025-01-06 20:05:46 -0800

    Reorganize directory structure
    ... in preparation for multiple days.

--- spec/day1_spec.rb
+++ spec/day1_spec.rb
@@ -1,11 +1,10 @@
-require "debug"
-require_relative "../one/solver"
+require_relative "../days/one/solver"
 
 RSpec.describe Day1Solver do
   subject { Day1Solver.new(inputfile) }
 
   context "given an input line" do
-    let(:inputfile) { "one/one-line-input" }
+    let(:inputfile) { "days/one/one-line-input" }
 
     it "converts it to integers stored in columns" do
       expect(subject.left_column).to eq [3]
@@ -15,7 +14,7 @@ RSpec.describe Day1Solver do
   end
 
   context "given a multi-line input file" do
-    let(:inputfile) { "one/mini-input" }
+    let(:inputfile) { "days/one/mini-input" }
 
     it "sorts each column's data" do
       expect(subject.sort.left_column).to eq  [1, 2, 3, 3, 3, 4]
